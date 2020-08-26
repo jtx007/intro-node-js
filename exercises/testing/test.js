dbb9340b37341a77d5f7cd43473f4fcc90d1f839
jtx007 <jamesjacobthomas7@gmail.com>
2020-08-26 01:27:12 -0400

    completed exercises

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,19 @@
 // write some tests
+const {findUser, deleteUser, users} = require('./users')
+
 describe('users', () => {
+  test('find user by id', () => {
+      findUser(1, (user) => {
+          expect(user.id).toBe(1)
+          done()
+      })    
+  })
+  test('delete user by id', () => {
+      deleteUser(1, (user) => {
+        expect(user.id).toBe(1)
+        expect(users).toHaveLength(users.length - 1)
+        done()
+      })
+  })
   
 })
